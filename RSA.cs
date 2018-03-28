582452bba1f503faf7e5185e7b18ff28cbe89c74
Ulceratio <36310673+Ulceratio@users.noreply.github.com>
2018-03-28 19:23:44 +0300

    Add files via upload

---
+++ RSA.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RSACipher
+{
+    class RSA
+    {
+        decimal n;
+        decimal p;
+        decimal q;
+        int e = 65537;
+        decimal phi;
+        decimal d;
+
+        public RSA()
+        {
+            Random nR = new Random(1539827);
+            do
+            {
+                p = nR.Next(100000, 120000);
+            }
+            while (!testPrimeNumbers(p, 5));
+            for (; ; )
+            {
+                q = nR.Next(100000, 120000);
+                if (testPrimeNumbers(q, 5))
+                {
+                    if (q != p)
+                    {
+                        break;
+                    }
+                }
+            }
+            n = p * q;
+            phi = (p - 1) * (q - 1);
+            lib newLib = new lib();
+            d = newLib.inverseElement(e, phi);
+        }
+
+        public string EncryptionOneByte(byte[] fileToEncrypt)
+        {
+            Console.WriteLine("Этап 1, создание массива decimal");
+            decimal[] result = new decimal[fileToEncrypt.Length];
+            lib newLib = new lib();
+            Console.WriteLine("Этап 2, шифрование");
+            Parallel.For(0, result.Length, (i, state) =>
+            {
+
+                result[i] = newLib.ModPow(Convert.ToDecimal(fileToEncrypt[i]), e, n);
+            }
+                );
+            Console.WriteLine("Этап 3, запись в строку");
+            string resultStr = String.Join("|", result);
+            return resultStr;
+        }
+
+        public void EncryptionFourBytes(byte[] fileToEncrypt, string path)
+        {
+            Console.WriteLine("Этап 1, добавление элементов");
+            fileToEncrypt = AddElements(fileToEncrypt);
+            Console.WriteLine("Этап 2, создание массива decimal");
+            decimal[] result = makeArrFromByteArr(fileToEncrypt);
+            lib newLib = new lib();
+            Console.WriteLine("Этап 3, шифрование");
+            Parallel.For(0, result.Length, (i, state) =>
+            {
+                result[i] = newLib.ModPow(result[i], e, n);
+            }
+                );
+            Console.WriteLine("Этап 4, запись в строку");
+            string resultStr = String.Join("|", result);
+            File.WriteAllText(path, resultStr);
+        }
+
+        public decimal[] makeArrFromByteArr(byte[] arr)
+        {
+            decimal[] result = new decimal[arr.Length / 4];
+            for (int i = 0, j = 0; j < result.Length; i += 4, j++)
+            {
+                result[j] = byteMerge(arr[i], arr[i + 1], arr[i + 2], arr[i + 3]);
+            }
+            return result;
+        }
+
+
+        public string DecryptionOneByte(string fileToDecrypt, string path)
+        {
+            Console.WriteLine("Этап 1,запись в массив decimal");
+            decimal[] result = fileToDecrypt.Split(new[] { "|" }, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
+            lib newLib = new lib();
+            Console.WriteLine("Этап 2,дешифрование");
+            Parallel.For(0, result.Length, (i, state) =>
+            {
+                result[i] = newLib.ModPow(result[i], d, n);
+            }
+                );
+            Console.WriteLine("Этап 3,запись в массив байт");
+            byte[] resByteArr = new byte[result.Length];
+            for (int i = 0; i < resByteArr.Length; i++)
+            {
+                resByteArr[i] = Convert.ToByte(result[i]);
+            }
+            Console.WriteLine("Этап 3 ,запись в файл");
+            File.WriteAllBytes(@path, resByteArr);
+            return null;
+        }
+
+        public void DecryptionFourBytes(string fileToDecrypt, string path)
+        {
+            Console.WriteLine("Этап 1,запись в массив decimal");
+            decimal[] result = fileToDecrypt.Split(new[] { "|" }, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
+            lib newLib = new lib();
+            Console.WriteLine("Этап 2,дешифрование");
+            Parallel.For(0, result.Length, (i, state) =>
+            {
+                result[i] = newLib.ModPow(result[i], d, n);
+            }
+                );
+            Console.WriteLine("Этап 3,запись в массив байт");
+            byte[] resByteArr = new byte[result.Length * 4];
+            for (int i = 0, j = 0; i < result.Length; i++, j += 4)
+            {
+                Tuple<byte, byte, byte, byte> tempTuple = divideBytes((uint)result[i]);
+                resByteArr[j] = tempTuple.Item1;
+                resByteArr[j + 1] = tempTuple.Item2;
+                resByteArr[j + 2] = tempTuple.Item3;
+                resByteArr[j + 3] = tempTuple.Item4;
+            }
+            Console.WriteLine("Этап 3,удаление добавленных элементов");
+            resByteArr = RemoveElements(resByteArr);
+            Console.WriteLine("Этап 4 ,запись в файл");
+            File.WriteAllBytes(@path, resByteArr);
+        }
+
+        public bool testPrimeNumbers(decimal n, decimal k)
+        {
+            bool result = true;
+            Random nR = new Random(16573);
+            lib newLib = new lib();
+            decimal a;
+            decimal temp1 = n - 1;
+            decimal s = 0;
+            decimal t = 0;
+            while (temp1 % 2 == 0)
+            {
+                s++;
+                temp1 /= 2;
+            }
+            t = temp1;
+            decimal x = 0;
+            for (int i = 0; i <= k; i++)
+            {
+                a = nR.Next(2, (int)(n - 1));
+                x = newLib.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                {
+                    continue;
+                }
+                for (int j = 0; j < s - 1; j++)
+                {
+                    x = Convert.ToInt32(newLib.ModPow(x, 2, n));
+                    if (x == 1)
+                    {
+                        result = false;
+                        break;
+                    }
+                    if (x == n - 1)
+                    {
+                        break;
+                    }
+                }
+                if (x != n - 1)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public byte[] getSequence()
+        {
+            return new byte[] { 251, 241, 239, 233, 229, 227, 223, 211, 199, 197, 193, 191, 181, 179, 173, 167, 163, 157, 151, 149, 139, 137, 131, 127, 113, 109, 107, 103, 101, 97, 89, 83, 79, 73, 71, 67, 61, 59, 53, 47, 43, 41, 37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2, 1 };
+        }
+
+        public byte[] reverseSequence(byte[] seq)
+        {
+            byte t;
+            for (int i = 0, j = seq.Length - 1; i < j; i++, j--)
+            {
+                t = seq[i];
+                seq[i] = seq[j];
+                seq[j] = t;
+            }
+            return seq;
+        }
+
+        public byte[] AddElements(byte[] curArr)
+        {
+            if (curArr.Length % 4 != 0)
+            {
+                int q = this.getSequence().Length + curArr.Length + 1;
+                while (true)
+                {
+                    if (q % 4 != 0)
+                    {
+                        q--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                int lngth = q - curArr.Length;
+                byte[] res = new byte[lngth];
+                byte[] seq = this.reverseSequence(this.getSequence());
+                res[0] = (byte)(q - curArr.Length);
+                for (int i = 0, j = 1; j < res.Length; i++, j++)
+                {
+                    res[j] = seq[i];
+                }
+                byte[] base_byte = res.Concat(curArr).ToArray();
+                return base_byte;
+            }
+            else
+            {
+                int q = this.getSequence().Length + curArr.Length;
+                byte[] res = new byte[this.getSequence().Length + 1];
+                res[0] = 56;
+                byte[] seq = this.reverseSequence(this.getSequence());
+                for (int i = 0, j = 1; j < res.Length; i++, j++)
+                {
+                    res[j] = seq[i];
+                }
+                byte[] result = res.Concat(curArr).ToArray();
+                return result;
+            }
+        }
+
+        public byte[] RemoveElements(byte[] curArr)
+        {
+            byte[] result;
+            int needToDelete = curArr[0] - 1;
+            int counter = 0;
+            byte[] seq = this.reverseSequence(this.getSequence());
+            for (int i = 1, j = 0; i < curArr.Length; i++, j++)
+            {
+                if (curArr[i] == seq[j] && counter < needToDelete)
+                {
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (counter != needToDelete)
+            {
+                throw new Exception("Encryption failed");
+            }
+            else
+            {
+                result = new byte[curArr.Length - (needToDelete + 1)];
+                Array.Copy(curArr,
+                (needToDelete + 1),
+                result,
+                0,
+                curArr.Length - (needToDelete + 1));
+                return result;
+            }
+        }
+        public uint byteMerge(byte b1, byte b2, byte b3, byte b4)
+        {
+            return ((uint)b1) << 24 | ((uint)b2) << 16 | ((uint)b3) << 8 | ((uint)b4);
+        }
+        public Tuple<byte, byte, byte, byte> divideBytes(UInt32 val)
+        {
+            return new Tuple<byte, byte, byte, byte>((byte)(val >> 24), (byte)((val >> 16) | (val << 8)), (byte)((val >> 8) | (val << 16)), (byte)(val & ~((~((uint)0)) << 8)));
+        }
+    }
+}
---
+++ lib.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSACipher
+{
+    class lib
+    {
+        public struct Remainder
+        {
+            public int a { get; set; }
+            public int m { get; set; }
+        }
+
+        public struct Factor
+        {
+            public int q { get; set; }
+            public int alpha { get; set; }
+        }
+
+        public decimal ModPow(decimal alpha, decimal beta, decimal n)  // (alpha ^ beta) mod n
+        {
+            decimal res = 1;
+            while (beta != 0)
+            {
+                if ((beta % 2) != 0)//beta%2!=0
+                {
+                    res = (res * alpha) % n;
+                    beta -= 1;
+                }
+                alpha = (alpha * alpha) % n;
+                beta /= 2; // beta/=2;
+            }
+            return res;
+        }
+
+
+        public int logA(int a, int b, int p)
+        {
+            int x = 0;
+            while (true)
+            {
+                if (ModPow(a, x, p) == b)
+                {
+                    return x;
+                }
+                else
+                {
+                    x++;
+                }
+            }
+        }
+
+        public int BinPow(int alpha, int beta)  // (alpha ^ beta)
+        {
+            int res = 1;
+            while (beta != 0)
+            {
+                if ((beta & 1) != 0)//beta%2!=0
+                {
+                    res = (res * alpha);
+                    beta -= 1;
+                }
+                alpha = (alpha * alpha);
+                beta = beta >> 1; // beta/=2;
+            }
+            return res;
+        }
+
+        public int logB(int alpha, int beta, int n) // alpha ^ x = beta (mod n)
+        {
+            int x = 0;
+            int k = Convert.ToInt32(Math.Sqrt(n));
+            int[] litleSteps = new int[k];
+            int[] bigSteps = new int[k];
+            for (int i = 0, j = 1; i < k; i++, j++)
+            {
+                bigSteps[i] = Convert.ToInt32(ModPow(alpha, j * k, n).ToString());
+                litleSteps[i] = (Convert.ToInt32(ModPow(alpha, j, n).ToString()) * beta) % n;
+            }
+            int sameEl = 0;
+            try
+            {
+                sameEl = Convert.ToInt32(string.Join("\n", bigSteps.Intersect(litleSteps)));
+            }
+            catch
+            {
+                for (int i = 0; i < bigSteps.Length; i++)
+                {
+                    for (int j = 0; j < litleSteps.Length; j++)
+                    {
+                        if (bigSteps[i] == litleSteps[j])
+                        {
+                            sameEl = bigSteps[i];
+                            break;
+                        }
+                    }
+                }
+            }
+            x = (Array.IndexOf(bigSteps, sameEl) + 1) * k - (Array.IndexOf(litleSteps, sameEl) + 1);
+            return x;
+        }
+
+        public decimal inverseElement(decimal a, decimal mod)
+        {
+            decimal result = ((gcdex((Int64)a, (Int64)mod).Item2 % mod) + mod) % mod;
+            return result;
+        }
+
+        public Tuple<long, long, long> gcdex(long m, long n)
+        {
+            long u = 0;
+            long v = 0;
+            long NOD = 0;
+            long a = m;
+            long b = n;
+            long u1 = 1, v1 = 0, u2 = 0, v2 = 1;
+            long q, r;
+            long temp;
+            while (b != 0)
+            {
+                q = a / b;
+                r = a % b;
+                a = b;
+                b = r;
+                temp = u2; u2 = u1 - q * u2; u1 = temp;
+                temp = v2; v2 = v1 - q * v2; v1 = temp;
+                a = u1 * m + v1 * n;
+                b = u2 * m + v2 * n;
+            }
+            NOD = a; u = u1; v = v1;
+            return Tuple.Create(NOD, u, v);
+        }
+
+        public int ChineseReminderTheorem(List<Remainder> equation)
+        {
+            int result = 0;
+            int M = 1;
+
+            foreach (var item in equation)
+            {
+                M *= item.m;
+            }
+
+            for (int i = 0; i < equation.Count; i++)
+            {
+                result += (int)(equation[i].a * (M / equation[i].m) * inverseElement((M / equation[i].m), equation[i].m));
+            }
+
+            return result % M;
+        }
+
+        public List<Factor> factorizationOfTheNumber(int p)
+        {
+            List<Factor> result = new List<Factor>();
+            foreach (var item in getSimpleNumbersEratosthenes(p))
+            {
+                result.Add(new Factor { q = item, alpha = 0 });
+            }
+            Parallel.For(1, result.Count, (i, state) =>
+            {
+                Factor temp = new Factor();
+                while ((p % result[i].q) == 0)
+                {
+                    temp.q = result[i].q;
+                    temp.alpha = result[i].alpha + 1;
+                    result[i] = temp;
+                    p /= result[i].q;
+                }
+            }
+            );
+            result = result.Where(x => x.alpha != 0).ToList();
+            return result;
+        }
+
+        public List<int> getSimpleNumbersEratosthenes(int thisTaskValue)
+        {
+            bool[] result = new bool[thisTaskValue + 1];
+            for (int i = 0; i < thisTaskValue + 1; i++)
+            {
+                result[i] = true;
+            }
+            int p = 2;
+            while (p * p < thisTaskValue)
+            {
+                for (int i = 2; p * i <= thisTaskValue; i++)
+                {
+                    result[p * i] = false;
+                }
+
+                for (int i = p; i <= thisTaskValue; i++)
+                {
+                    if (result[i] == true && i > p)
+                    {
+                        p = i;
+                        break;
+                    }
+                }
+            }
+            List<int> resultNums = new List<int>();
+            for (int i = 1; i < result.Length; i++)
+            {
+                if (result[i])
+                {
+                    resultNums.Add(i);
+                }
+            }
+            return resultNums;
+        }
+
+        public int PohligHellmanAlgorithm(int a, int b, int p) // a ^ x = b (mod p)
+        {
+            List<List<int>> r = new List<List<int>>();
+            List<Factor> factors = factorizationOfTheNumber(p - 1);
+            foreach (var item in factors)
+            {
+                Console.WriteLine(item.q + " " + item.alpha);
+            }
+            for (int i = 0; i < factors.Count; i++)
+            {
+                List<int> tmp = new List<int>();
+                r.Add(tmp);
+                for (int j = 0; j <= factors[i].q - 1; j++)
+                {
+                    tmp.Add((int)ModPow(a,
+                        (j * ((p - 1) / factors[i].q)),
+                        p));
+                }
+            }
+            foreach (var item in r)
+            {
+                foreach (var item1 in item)
+                {
+                    Console.Write(item1 + " ");
+                }
+                Console.WriteLine();
+            }
+            List<Remainder> remainders = new List<Remainder>();
+            Parallel.For(0, factors.Count, (i, state) =>
+            {
+                List<int> x = new List<int>();
+                int pow = 0;
+                int temp = 0;
+                for (int xi = 0; xi <= factors[i].q - 1; xi++)
+                {
+                    for (int j = 0, k = 0; j < x.Count; j++, k++)
+                    {
+                        if (j >= 0)
+                        {
+                            pow -= x[j] * Convert.ToInt32(BinPow(factors[i].q, j));
+                        }
+                        else
+                        {
+                            pow -= x[j] * Convert.ToInt32(Math.Pow(factors[i].q, j));
+                        }
+                        // Console.WriteLine("j={0}", xi);
+                    }
+                    temp = (int)ModPow((int)(b * Math.Pow(a, pow)),
+                        (int)((p - 1) / Math.Pow(factors[i].q, xi + 1)),
+                        p);
+                    //  Console.WriteLine("temp={0}", temp);
+                    for (int j = 0; j < r.ElementAt(i).Count; j++)
+                    {
+                        if (r.ElementAt(i).ElementAt(j) == temp)
+                        {
+                            x.Add(j);
+                            break;
+                        }
+                    }
+                }
+                Remainder nRemainder = new Remainder();
+                nRemainder.a = 0;
+                nRemainder.m = BinPow(factors[i].q, factors[i].alpha);
+                for (int j = 0; j < x.Count; j++)
+                {
+                    if (j >= 0)
+                    {
+                        nRemainder.a += x[j] * Convert.ToInt32(BinPow(factors[i].q, j));
+                    }
+                    else
+                    {
+                        nRemainder.a += x[j] * Convert.ToInt32(Math.Pow(factors[i].q, j));
+                    }
+                    Console.WriteLine("i={0}  j={1}", i, j);
+                }
+                remainders.Add(nRemainder);
+            }
+            );
+            foreach (var item in remainders)
+            {
+                Console.WriteLine("a={0} m={1}", item.a, item.m);
+            }
+            int result = ChineseReminderTheorem(remainders);
+            return result;
+        }
+    }
+}
+
